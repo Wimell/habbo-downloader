e2f3ad0986c0027c25ea541684c6d1a79f339404
Wimell <99610715+Wimell@users.noreply.github.com>
2023-11-28 14:45:04 -0500

    Added JSON Conversion

---
+++ scripts/externalTextsToJson.js
@@ -0,0 +1,18 @@
+// convertXmlToJson.js
+const { readFile, writeFile } = require('fs/promises');
+const { parseStringPromise } = require('xml2js');
+
+async function convertXmlToJson(xmlFilePath, jsonFilePath) {
+  try {
+    const xml = await readFile(xmlFilePath, 'utf8');
+    const result = await parseStringPromise(xml);
+    await writeFile(jsonFilePath, JSON.stringify(result, null, 2), 'utf8');
+    console.log(`Converted ${xmlFilePath} to JSON.`);
+  } catch (error) {
+    console.error('Error converting XML to JSON:', error);
+  }
+}
+
+const xmlFilePath = './resource/gamedata/external_flash_texts.txt';
+const jsonFilePath = './resource/gamedata/ExternalTexts.json';
+convertXmlToJson(xmlFilePath, jsonFilePath);
