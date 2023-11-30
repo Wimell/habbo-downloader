2a808a62da3f367ee7758ca54cbdc505758144dd
Wes Wimell <heywimell@gmail.com>
2023-11-30 13:54:06 -0500

    Added Sprite Loader

---
+++ atlas_loader/src/builders/OffsetBuilder.ts
@@ -0,0 +1,104 @@
+import fs from "fs";
+import {xml2js} from "xml-js";
+
+export class OffsetBuilder {
+
+    public buildFurnitureOffset(assetName: string, outputPath: string) {
+        return new Promise<void>(async resolve => {
+            let assetsXml: any = fs.readFileSync(`${outputPath}/${assetName}/${assetName}_assets.bin`);
+            assetsXml = xml2js(assetsXml, {compact: false});
+            let spritesheet: any = fs.readFileSync(`${outputPath}/${assetName}/${assetName}.json`);
+            spritesheet = JSON.parse(spritesheet);
+
+            if (spritesheet?.meta?.image) {
+                spritesheet.meta.image = `${assetName}.png`;
+            }
+
+            //Metadata
+            let has32: boolean = false;
+
+            let modifiedAssets: {}[] = [];
+            let sourceAssets: any[] = [];
+
+            assetsXml.elements[0]?.elements.forEach((asset) => {
+
+                if(asset.attributes !== undefined) {
+                    const name: string = asset.attributes.name;
+
+                    try {
+                        const splittedName: string[] = asset.attributes.name.split("_");
+                        splittedName.splice(splittedName.length - 4, 4);
+
+                        //Check if it has a 32 sprite
+                        if (asset.attributes.name.includes("_32_")) {
+                            has32 = true;
+                        }
+
+                        const className = splittedName.join("_");
+                        console.log(spritesheet.frames[asset.attributes.name]);
+                        if (spritesheet.frames[asset.attributes.name] !== undefined) {
+                            const { spriteSourceSize } = spritesheet.frames[asset.attributes.name];
+                            spriteSourceSize.x = -parseInt(asset.attributes.x);
+                            spriteSourceSize.y = -parseInt(asset.attributes.y);
+                            spritesheet.frames[asset.attributes.name].flipH = asset.attributes.flipH !== undefined;
+                            modifiedAssets.push(asset);
+                        } else if(asset.attributes.source !== undefined) {
+                            sourceAssets.push(asset);
+                        }
+                    } catch (e) {
+                        console.log(e);
+                        const splittedName: string[] = name.split("_");
+                        if (splittedName[splittedName.length - 4] !== "32") {
+                            console.log("\x1b[0m", ">>", "\x1b[31m", `Error finding frame ${name}`, "\x1b[0m");
+                        }
+                    }
+                }
+            });
+            sourceAssets.forEach((asset) => {
+                try {
+                    const splittedName: string[] = asset.attributes.name.split("_");
+                    splittedName.splice(splittedName.length - 4, 4);
+                    const className = splittedName.join("_");
+
+                    spritesheet.frames[asset.attributes.name] = {
+                        "frame": {
+                            "x": spritesheet.frames[asset.attributes.source].frame.x,
+                            "y": spritesheet.frames[asset.attributes.source].frame.y,
+                            "w": spritesheet.frames[asset.attributes.source].frame.w,
+                            "h": spritesheet.frames[asset.attributes.source].frame.h
+                        },
+                        "sourceSize": {
+                            "w": spritesheet.frames[asset.attributes.source].sourceSize.w,
+                            "h": spritesheet.frames[asset.attributes.source].sourceSize.h
+                        },
+                        "spriteSourceSize": {
+                            "x": -parseInt(asset.attributes.x),
+                            "y": -parseInt(asset.attributes.y),
+                            "w": spritesheet.frames[asset.attributes.source].spriteSourceSize.w,
+                            "h": spritesheet.frames[asset.attributes.source].spriteSourceSize.h
+                        },
+                        "rotated": false,
+                        "trimmed": true,
+                        "flipH": asset.attributes.flipH !== undefined
+                    }
+                    modifiedAssets.push(asset);
+                }catch (e) {
+
+                }
+            });
+
+            //Adjust the metadata
+            if (has32) {
+                spritesheet.meta.sizes = ["64", "32"];
+            } else {
+                spritesheet.meta.sizes = ["64"];
+            }
+
+
+            fs.writeFile(`${outputPath}/${assetName}/${assetName}.json`, JSON.stringify(spritesheet), () => {
+                resolve();
+            });
+        });
+    }
+
+}
